a45c564f3692170e33985de08508e8783239a1e8
ainhoavegasanchez <ainhoavega99@gmail.com>
2024-05-03 17:13:38 +0200

    cambio

--- server.ts
+++ server.ts
@@ -5,37 +5,37 @@ import { fileURLToPath } from 'url';
 import { dirname, join, resolve } from 'path';
 import bootstrap from './src/main.server';
 import cors from 'cors';
-import process from 'process';
+
 
 
 // The Express app is exported so that it can be used by serverless Functions.
-export function app(): express.Express {
-  const server = express();
+
+  const app = express();
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
   const indexHtml = join(serverDistFolder, 'index.server.html');
 
   const commonEngine = new CommonEngine();
 
-  server.set('view engine', 'html');
-  server.set('views', browserDistFolder);
+  app.set('view engine', 'html');
+  app.set('views', browserDistFolder);
 
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(browserDistFolder, {
+  app.get('*.*', express.static(browserDistFolder, {
     maxAge: '1y'
   }));
 
 
-  const corsOptions = {
+  /*const corsOptions = {
     origin: '*', 
     credentials: true, 
     optionsSuccessStatus: 200, 
  };
- server.use(cors(corsOptions));
+ app.use(cors(corsOptions));
   // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
+  app.get('*', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
 
     commonEngine
@@ -48,19 +48,11 @@ export function app(): express.Express {
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
-  });
-
-  return server;
-}
-
-function run(): void {
-  const port =  4200;
-
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
+  });*/
+  const port = process.env["PORT"] || 4000;
+  app.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
-}
 
-run();
+
+  
